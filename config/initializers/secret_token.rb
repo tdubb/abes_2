403903853a6b265155a624c7214e58e1b6ad5434
Tyler Wasden <twasden@gmail.com>
2013-04-04 04:40:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abes2::Application.config.secret_token = '0d3d90677ff8d611469ef5f14c309eb8b98fdf3cc0b7a5e894387070e2641da2bbc490be563075079093916a23011f6ad5d916ab8e26c414fea05eb78378df9d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Abes2::Application.config.session_store :cookie_store, key: '_abes_2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Abes2::Application.config.session_store :active_record_store
